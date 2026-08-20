cf37e3a3c7d88f633054a396670378c8c869de69
euudanilo <daniloalvesdafonseca@gmail.com>
2026-08-20 15:19:47 -0300

    chore: inicializa projeto com Spring Initializr

---
+++ src/main/java/com/danilo/curriculoautomatizador/CurriculoautomatizadorApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.curriculoautomatizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoautomatizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculoautomatizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/curriculoautomatizador/CurriculoautomatizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.curriculoautomatizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoautomatizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
